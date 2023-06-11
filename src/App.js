5fffffbbb41743a7c792af96ecdfde9490aef1da
Ali Berat Tatli <berat.tatli@prodyum.com>
2023-06-12 00:59:38 +0300

    Covid-19 Engine With Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState,} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [veri,setVeri] = useState("");
+  const [tarih,setTarih] = useState();
+  useEffect(()=>{
+
+    axios.get("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json")
+    .then(res=>setVeri(res.data[tarih]))
+    .catch(err=>console.log(err))
+
+  }, [veri,tarih])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto mt-4">
+            <h2 className="text-center text-white display-3">TURKEY COVID-19 SEARCH ENGINE
+            </h2>
+            <input type="text" placeholder="GG/AA/YY" className="form-control"
+            onChange={(e)=>setTarih(e.target.value)}/>
+
+            <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">Test Count</th>
+                    <th scope="col">Infected Count</th>
+                    <th scope="col">Number Of Death</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="text-white">
+                    <td>{veri===undefined ? "Data is waiting" : veri.totalTests}</td>
+                    <td >{veri===undefined ? "Data is waiting" : veri.totalPatients}</td>
+                    <td>{veri===undefined ? "Data is waiting" : veri.totalDeaths}</td>
+                  </tr>
+                </tbody>
+              </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
